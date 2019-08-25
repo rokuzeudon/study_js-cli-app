88c6135e85430dabee9e47499d9653543b8ff6ae
rokuzeudon <rokuzeudon@gmail.com>
2019-08-26 06:07:48 +0900

    nodecli: 実験 コマンドライン引数からファイルパスを取得

--- nodecli/main.js
+++ nodecli/main.js
@@ -1,2 +1,8 @@
-// コンソールにコマンドライン引数を出力する
-console.log(process.argv);
+// commanderモジュールをprogramとしてインポートする
+const program = require("commander");
+// コマンドライン引数をcommanderでパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
