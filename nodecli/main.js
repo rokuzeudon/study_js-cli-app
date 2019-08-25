433d53909827d1884332e32f1c2c10e774ee54ee
rokuzeudon <rokuzeudon@gmail.com>
2019-08-26 06:14:02 +0900

    nodecli: fsモジュールでの読込 エラーハンドリング処理追加

--- nodecli/main.js
+++ nodecli/main.js
@@ -9,5 +9,11 @@ const filePath = program.args[0];
 
 // ファイルをUTF-8として非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
+  if (err) {
+    console.error(err.message);
+    // 終了ステータス 1（一般的なエラー）としてプロセスを終了する
+    process.exit(1);
+    return;
+  }
   console.log(file);
 });
