6e49ccce561bad317a7f65ce177aa26c321f7630
rokuzeudon <rokuzeudon@gmail.com>
2019-08-27 06:09:06 +0900

    nodecli: コメント追記

--- nodecli/main.js
+++ nodecli/main.js
@@ -7,6 +7,7 @@ const marked = require("marked");
 
 // コマンドライン引数からファイルパスを取得する
 program.parse(process.argv);
+// ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
 
 // ファイルをUTF-8として非同期で読み込む
